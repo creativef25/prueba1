828856870aaac5c6ebb4761696068910539cb1b1
creativef25 <creativef25@gmail.com>
2017-09-04 10:06:40 -0500

    proyecto CRUD

---
+++ src/CrudBundle/Entity/Usuario.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace CrudBundle\Entity;
+
+/**
+ * Usuario
+ */
+class Usuario
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $nombre;
+
+    /**
+     * @var string
+     */
+    private $apellidoPaterno;
+
+    /**
+     * @var string
+     */
+    private $apellidoMaterno;
+
+    /**
+     * @var int
+     */
+    private $edad;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     *
+     * @return Usuario
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return string
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set apellidoPaterno
+     *
+     * @param string $apellidoPaterno
+     *
+     * @return Usuario
+     */
+    public function setApellidoPaterno($apellidoPaterno)
+    {
+        $this->apellidoPaterno = $apellidoPaterno;
+
+        return $this;
+    }
+
+    /**
+     * Get apellidoPaterno
+     *
+     * @return string
+     */
+    public function getApellidoPaterno()
+    {
+        return $this->apellidoPaterno;
+    }
+
+    /**
+     * Set apellidoMaterno
+     *
+     * @param string $apellidoMaterno
+     *
+     * @return Usuario
+     */
+    public function setApellidoMaterno($apellidoMaterno)
+    {
+        $this->apellidoMaterno = $apellidoMaterno;
+
+        return $this;
+    }
+
+    /**
+     * Get apellidoMaterno
+     *
+     * @return string
+     */
+    public function getApellidoMaterno()
+    {
+        return $this->apellidoMaterno;
+    }
+
+    /**
+     * Set edad
+     *
+     * @param integer $edad
+     *
+     * @return Usuario
+     */
+    public function setEdad($edad)
+    {
+        $this->edad = $edad;
+
+        return $this;
+    }
+
+    /**
+     * Get edad
+     *
+     * @return int
+     */
+    public function getEdad()
+    {
+        return $this->edad;
+    }
+}
+
---
+++ src/CrudBundle/Form/UsuarioType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace CrudBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UsuarioType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('nombre')->add('apellidoPaterno')->add('apellidoMaterno')->add('edad')        ;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'CrudBundle\Entity\Usuario'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'crudbundle_usuario';
+    }
+
+
+}
