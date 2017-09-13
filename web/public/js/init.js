7c7b3d95abe2790e43d192cc7888353337a394b7
creativef25 <creativef25@gmail.com>
2017-09-13 00:27:31 -0500

    se agregaron estilios css y detalles en los botones

--- web/public/js/init.js
+++ web/public/js/init.js
@@ -4,6 +4,9 @@
     $('.button-collapse').sideNav();
     $('.slider').slider();
     $('select').material_select();
+    $('.tooltipped').toolip({
+      delay: 50
+    });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
